f11d3e17944e7127bc142c0428852ac9e2d1efb1
awattsup <adamwatts1994@gmail.com>
2023-02-09 13:04:28 +0800

    fix multiprocessing with multiprocess

--- tests/check_outputs.py
+++ tests/check_outputs.py
@@ -1,4 +1,18 @@
 import numpy as np
+import os
+
+
+output_dir = "/home/runner/work/gist-geckos-test/gist-geckos-test/NGC0000"
+
+
+assert os.path.isdir(output_dir), "Output dir not created"
+
+
+assert os.path.isdir(output_dir), "Output dir not created"
+
+
+
+
 
 
 
